316ee0699706d99b8a71fd531e2b36f4d69e134d
Andreas Stassivik <andreas@stassi.net>
2020-03-08 18:04:29 -0700

    decrement

---
+++ src/decrement.ts
@@ -0,0 +1,9 @@
+/**
+ * @description Decrement by 1.
+ * @param {number} n Number to be decremented.
+ * @returns {number} Number decremented by 1.
+ * @see #increment
+ */
+export default function decrement(n: number): number {
+  return n - 1
+}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
+export { default as decrement } from './decrement'
 export { default as increment } from './increment'
 export { default as sum } from './sum'
