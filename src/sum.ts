dd618d72e14de6a8434d4fe60f546f72bd906f54
Andreas Stassivik <andreas@stassi.net>
2020-04-02 16:43:07 -0700

    unary signature

--- src/sum.ts
+++ src/sum.ts
@@ -2,11 +2,11 @@ import add from './add'
 
 /**
  * @description Compute the sum of numbers.
- * @param {...number} n Numbers to be summed.
+ * @param {number[]} n Numbers to be summed.
  * @returns {number} Sum of numbers.
  * @see #increment
  */
-export default function sum(...n: number[]): number {
+export default function sum(n: number[]): number {
   return n.reduce((prev, current) => {
     return add([prev, current])
   }, 0)
