78dedd0dd5d692759559126bb583b5ebd35ee32f
Andreas Stassivik <andreas@stassi.net>
2020-04-02 15:38:13 -0700

    identity

---
+++ src/identity.ts
@@ -0,0 +1,8 @@
+/**
+ * @description Return the input.
+ * @param {*} input The value to be returned.
+ * @returns {*} The input parameter.
+ * */
+export default function identity(input: any): any {
+  return input
+}
