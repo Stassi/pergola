2e13c58c77945c78b8e76c119b4a769445977057
Andreas Stassivik <andreas@stassi.net>
2020-04-02 21:01:34 -0700

    implement reduce

--- src/sum.ts
+++ src/sum.ts
@@ -1,13 +1,16 @@
 import add from './add'
+import reduce from './reduce'
 
 /**
  * @description Compute the sum of numbers.
- * @param {number[]} n Numbers to be summed.
+ * @param {number[]} collection Numbers to be summed.
  * @returns {number} Sum of numbers.
  * @see #increment
  */
-export default function sum(n: number[]): number {
-  return n.reduce((prev, current) => {
-    return add([prev, current])
-  }, 0)
+export default function sum(collection: number[]): number {
+  return reduce({
+    collection,
+    initialValue: 0,
+    reducer: (accumulator: number, value: number) => add([accumulator, value]),
+  })
 }
