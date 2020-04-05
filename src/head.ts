84b743380217d8b259ac72df5366e1b6b2da6207
Andreas Stassivik <andreas@stassi.net>
2020-04-05 01:42:17 -0700

    related methods

--- src/head.ts
+++ src/head.ts
@@ -2,6 +2,7 @@
  * @description Find the first element.
  * @param {*[]} collection
  * @returns {*} First element.
+ * @see #tail
  */
 export default function head([element]: any[]): any {
   return element
--- src/tail.ts
+++ src/tail.ts
@@ -2,6 +2,7 @@
  * @description Find the tail elements following the head.
  * @param {*[]} collection
  * @returns {*[]} Tail elements.
+ * @see #head
  */
 export default function tail([, ...elements]: any[]): any {
   return elements
